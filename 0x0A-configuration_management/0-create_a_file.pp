fff72b0c19d0dad98f1b53934e23367a80b6770b
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 09:32:39 -0700

    pupplet is okay

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#Using Puppet, create a file in /tmp
+#Using Puppet, create a file in /tmp.
 
 file {'school':
-	name => '/tmp/school',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+  name    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
