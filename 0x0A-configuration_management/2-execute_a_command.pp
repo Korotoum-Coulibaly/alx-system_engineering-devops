33afb0789e8703295cb31c4fd780a36c79839595
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 09:35:45 -0700

    pupplet is okay

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #Using Puppet, create a manifest that kills a process named killmenow
 
 exec { 'killemenow':
-  command  => 'pkill killmenow',
-  path     => '/usr/local/bin/:/usr/bin:/bin/',
+  command => 'pkill killmenow',
+  path    => '/usr/local/bin/:/usr/bin:/bin/',
 }
