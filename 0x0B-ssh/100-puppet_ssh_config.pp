da627c96f045a7954a8d7d0134e0a9b8181a7fa8
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 11:20:30 -0700

    advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'passwordAuthentication no'
+  line   => '    passwordAuthentication no'
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  line   => '   IdentityFile ~/.ssh/school',
 }
