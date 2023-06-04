86cbd0127050329673577c53fcddc01e529a21bd
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-04 01:15:05 +0000

    add readme file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Python script that, using Rest API, for a given employee ID, returns information"""
+"""Python script that, using Rest API, for a given employee ID, returns information """
 import requests
 import sys
 
 if __name__ == "__main__":
     _id = sys.argv[1]
     usr_url = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
             _id)
 
     user = requests.get(usr_url).json()
-    infos = requests.get(url).json()
+    todos = requests.get(todos_url).json()
 
-    compteur = 0
+    completed = 0
     total = 0
     completed_tasks = []
 
-    for task in infos:
+    for task in todos:
         total += 1
-        if task.get("compteur") is True:
-            compteur += 1
+        if task.get("completed") is True:
+            completed += 1
             completed_tasks.append(task.get("title"))
 
     sentence = "Employee {} is done with tasks({}/{}):"
-    print(sentence.format(user.get("name"), compteur, total))
+    print(sentence.format(user.get("name"), completed, total))
     for task in completed_tasks:
         print("\t {}".format(task))
